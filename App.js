cd5133d0c5bad8d1a30666b770ee62bfd4deaa34
usmanalameer <74920143+usmanalameer@users.noreply.github.com>
2022-09-30 16:06:59 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import AppBar from './components/AppBar';
+import BasicCard from './components/Card';
+
+function App() {
+
+
+  return (
+  <>
+  <AppBar />
+<BasicCard />
+
+  </>  
+  )
+
+  }
+export default App;
